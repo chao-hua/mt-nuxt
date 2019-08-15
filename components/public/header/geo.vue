0d4f677701f2c5c5a51fbe5279d9b7d35ddbe618
chao-hua <chaohua@outlook.com>
2019-08-15 16:07:07 +0800

    update

---
+++ components/public/header/geo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location" />西安市
+    <nuxt-link to="/changeCity" class="changeCity">切换城市</nuxt-link>
+    [北京,上海，深圳]
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/public/header/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <geo />
+  </header>
+</template>
+
+<script>
+import Geo from './geo'
+export default {
+  components: {
+    Geo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/public/header/index.scss";
+</style>
