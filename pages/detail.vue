8eda867f3aad61b19e6081051e23f8c25d0d2e8b
chao-hua <chaohua@outlook.com>
2019-09-06 17:30:14 +0800

    update

--- pages/detail.vue
+++ pages/detail.vue
@@ -20,7 +20,7 @@
         <list v-if="login" :list="list" />
         <div v-else class="deal-need-login">
           <img src="//p0.meituan.net/codeman/56a7d5abcb5ce3d90fc91195e5b5856911194.png" alt="登录查看" />
-          <span>请登录后查看详细团购优惠</span>
+          <span>请登录后查看详细优惠</span>
           <el-button type="primary" round>
             <a href="/login">立即登录</a>
           </el-button>
